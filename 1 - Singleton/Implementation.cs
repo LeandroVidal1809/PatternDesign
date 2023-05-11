44a70e3c2b1d6ed0c70c5e9bc258355e0aa8545d
Leandro Vidal <leandrovidal92@gmail.com>
2023-05-11 16:57:53 -0300

    Singleton

---
+++ 1 - Singleton/Implementation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+   public class Logger
+    {
+        private static  Logger? _instance;
+
+        /// <summary>
+        /// instance
+        /// </summary>
+        public static Logger Instance
+        {
+            get {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        protected Logger() 
+        {
+        
+        }
+        
+        public void Log(string message) 
+        {
+            Console.WriteLine($"Message: {message} ");
+        }
+    }
+}
---
+++ 1 - Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using Singleton;
+using System.Security.Claims;
+
+//Singleton:
+//-Creational Pattern
+//- Ensure that an class only has one instance and to provide global access to it 
+//- Example : Logger
+//- Class must be private or protect and contain and property (static) that
+//  return the instance for ensure only one instance.
+
+Console.Title = "Singleton Pattern";
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are the same");
+}
+
+instance1.Log("Logging instance 1");
+instance2.Log("Logging instance 2");
+Logger.Instance.Log("Logging instance");
+
+Console.ReadLine();
